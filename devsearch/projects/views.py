167ff05f905ef0a9b27fdad898a3e45e3885f425
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-23 20:18:36 -0400

    Installation & Setup

---
+++ devsearch/projects/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def projects(request):
+    return HttpResponse('Here are our projects')
+
+def project(request, pk):
+    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
