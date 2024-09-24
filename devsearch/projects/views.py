044f241d8b5f7ba1bc0ea67bb66e05870a09e597
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-24 08:49:38 -0400

    Rendering Data to Templates

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -3,8 +3,36 @@ from django.http import HttpResponse
 
 
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website.'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work.'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community.'
+    }
+]
+
+
+
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'projects'
+    number = 3
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
