a81fdb8b08906819fd32beebf3a2f74141ee93d3
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-23 20:55:51 -0400

    Templates & Template Inheritance

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
+    return render(request, 'projects/single-project.html')
