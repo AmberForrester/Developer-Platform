3c014431bdb06471cf0fa82510644d60b2ae168f
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-25 23:09:53 -0400

    Model Forms

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,27 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
-
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website.'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work.'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community.'
-    }
-]
-
+from .forms import ProjectForm
 
 
 
@@ -33,3 +13,8 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
