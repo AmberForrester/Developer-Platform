7311cc858facbb06ed81f6cc8eecf321be93f044
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-26 12:06:01 -0400

    Staticfiles

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) - For production. 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # - For production. 
