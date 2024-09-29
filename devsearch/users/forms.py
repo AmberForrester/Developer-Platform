0464f29512934514d1be34f7d08a46954f56fc95
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-28 21:55:11 -0400

    Styling Login & Register Page

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -12,4 +12,10 @@ class CustomUserCreationForm(UserCreationForm):
             'password1',
             'password2' 
         ]
-        labels = {'first_name': 'Full Name',}
+        labels = {'first_name': 'Full Name'}
+        
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
