e3fc3099c9c219303675d09c28260a3a29d131b4
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-29 17:14:10 -0400

    Add, Edit, & Delete add skills/projects

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -29,7 +29,7 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
-            return redirect('projects')
+            return redirect('account')
         
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
@@ -45,7 +45,7 @@ def updateProject(request, pk):
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect('account')
         
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
@@ -60,4 +60,4 @@ def deleteProject(request, pk):
         return redirect('projects')
     
     context = {'object': project}
-    return render(request, 'projects/delete_template.html', context)
+    return render(request, 'delete_template.html', context)
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm, ProfileForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 
 
 
@@ -81,6 +81,7 @@ def userProfile(request, pk):
     return render(request, 'users/user-profile.html', context)
 
 
+
 @login_required(login_url='login')
 def userAccount(request):
     profile = request.user.profile
@@ -91,6 +92,8 @@ def userAccount(request):
     
     context = {'profile': profile, 'skills': skills, 'projects': projects}
     return render(request, 'users/account.html', context)
+
+
 
 @login_required(login_url='login')
 def editAccount(request):
@@ -106,3 +109,55 @@ def editAccount(request):
     
     context = {'form': form}
     return render(request, 'users/profile_form.html', context)
+
+
+
+@login_required(login_url='login')
+def createSkill(request):
+    profile = request.user.profile
+    form = SkillForm()
+    
+    if request.method == 'POST':
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            skill = form.save(commit=False)
+            skill.owner = profile 
+            skill.save()
+            messages.success(request, 'Congrats! Your skill was added successfully!')
+            return redirect('account')
+            
+    context = {'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+
+@login_required(login_url='login')
+def updateSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    form = SkillForm(instance=skill)
+    
+    if request.method == 'POST':
+        form = SkillForm(request.POST, instance=skill)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Congrats! Your skill was updated successfully!')
+            return redirect('account')
+            
+    context = {'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+
+@login_required(login_url='login')
+def deleteSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    
+    if request.method == 'POST':
+        skill.delete()
+        messages.success(request, 'Congrats! Your skill was deleted successfully!')
+        return redirect('account')
+    
+    context = {'object': skill}
+    return render(request, 'delete_template.html', context)
