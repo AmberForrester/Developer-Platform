fe6c734c90abbe291e63c8c0a8306bd5ebc1f72d
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-28 22:51:35 -0400

    User Edit Profile

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 
 
@@ -54,7 +54,7 @@ def registerUser(request):
             messages.success(request, 'User account was created!')
             
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
         
         else:
             messages.error(request, 'Sorry, an error has occurred during registration.')
@@ -91,3 +91,18 @@ def userAccount(request):
     
     context = {'profile': profile, 'skills': skills, 'projects': projects}
     return render(request, 'users/account.html', context)
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            
+            return redirect('account')
+    
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
