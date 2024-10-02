af25bca093c460c50d2512cc0001ad145ff9b314
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-02 15:19:43 -0400

    Sending Welcome Emails!

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -1,8 +1,9 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
 from django.contrib.auth.models import User
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 
 # @receiver(post_save, sender=Profile) 
 
@@ -17,6 +18,18 @@ def createProfile(sender, instance, created, **kwargs):
             name=user.first_name,
         )
         
+        subject = 'Welcome To DevSearch'
+        message = 'Thank you for signing up, we are glad you are here!'
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER, 
+            [profile.email],
+            fail_silently=False,
+        )
+        
+
 
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
