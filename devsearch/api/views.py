6a289074f27ad34427ab910fe630fb6cd2a01cbe
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-02 23:26:42 -0400

    Simple API setup

---
+++ devsearch/api/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET', 'POST'])
+def getRoutes(request):
+    
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
+
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
+    ]
+    
+    return Response(routes)
