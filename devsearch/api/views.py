6284e746c9cbe7d14fa7342846140c7c8512046d
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-03 10:41:46 -0400

    JSON Web Tokens Authentication

--- devsearch/api/views.py
+++ devsearch/api/views.py
@@ -1,7 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
+
+from api import serializers
 
 
 @api_view(['GET', 'POST'])
