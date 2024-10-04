577d06dd63d06dd122d5070ebdda2954b8b9dd81
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-04 11:14:29 -0400

    Sending Data from Frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,27 @@
+let loginBtn = document.getElementById('login-btn')
+let logoutBtn = document.getElementById('logout-btn')
+
+let token = localStorage.getItem('token')
+
+if (token) {
+
+    loginBtn.remove()
+}
+
+else {
+
+    logoutBtn.remove()
+}
+
+logoutBtn.addEventListener('click', (e) => {
+
+    e.preventDefault()
+    localStorage.removeItem('token')
+    window.location = 'file:///C:/code/Developer-Platform/frontend/login.html'
+})
+
+
+
 
 let projectsUrl = 'http://127.0.0.1:8000/api/projects/' // Set the endpoint to getting the projects
 
@@ -7,8 +31,8 @@ let getProjects = () => {   // Make a request
         .then(response => response.json())   // Convert the request into JSON data.
         .then(data => {
 
-            //console.log(data)
-            buildProjects(data)
+            console.log(data)
+            buildProjects(data)   // Trigger this function
         })
 }
 
@@ -19,7 +43,10 @@ let buildProjects = (projects) => {
     let projectsWrapper = document.getElementById('projects--wrapper')
     //console.log('projectsWrapper:', projectsWrapper)
 
+    projectsWrapper.innerHTML = ''
+
     for (let i =0; projects.length > i; i++){
+
         let project = projects[i]
         //console.log(project)
 
@@ -30,8 +57,8 @@ let buildProjects = (projects) => {
                     <div>
                         <div class='card--header'>
                             <h3>${project.title}</h3>
-                            <strong class='vote--option' >&#43;</strong>
-                            <strong class='vote--option' >&#8722;</strong>
+                            <strong class='vote--option' data-vote='up' data-project='${project.id}' >&#43;</strong>
+                            <strong class='vote--option' data-vote='down' data-project='${project.id}' >&#8722;</strong>
                         </div>
 
                         <i>${project.vote_ratio}% Positive Feedback </i>
@@ -44,6 +71,48 @@ let buildProjects = (projects) => {
         projectsWrapper.innerHTML += projectCard
     }
 
+    addVoteEvents()   // Trigger the function to add the event listeners
+
 }
+
+
+
+// Add an eventListener for the vote and trigger a POST request.
+let addVoteEvents = () => {
+    let voteBtns = document.getElementsByClassName('vote--option')
+    //console.log('VOTE BUTTONS:', voteBtns)
+
+    for (let i=0; voteBtns.length > i; i++){
+
+        voteBtns[i].addEventListener('click', (e) => {
+            let token = localStorage.getItem('token')
+            //console.log('Vote was clicked:', i)
+            
+            let vote = e.target.dataset.vote
+            let project = e.target.dataset.project
+            //console.log('PROJECT:', project, 'VOTE:', vote)
+
+            fetch(`http://127.0.0.1:8000/api/projects/${project}/vote/`, {
+
+                method: 'POST', 
+                headers: {
+
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                },
+
+                body: JSON.stringify({'value': vote})
+            })
+
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data)
+                getProjects()   // Trigger the function to retrieve the data.
+            })
+        })
+    }
+}
+
+
 
 getProjects()   // Trigger the function to retrieve the data.
